7726eff7ac332c49e7b93cebb8a7a820113adfb4
AakashShahi <129369974+AakashShahi@users.noreply.github.com>
2024-04-09 20:58:04 +0545

    map of task kotlin

--- Desktop/FlutterProjects/MyApp/app/src/main/java/com/example/myapp/Basics.kt
+++ Desktop/FlutterProjects/MyApp/app/src/main/java/com/example/myapp/Basics.kt
@@ -9,5 +9,21 @@ fun main() {
   var b= ArrayList<Any>()
   b.add(1)
   b.add("ram")
+  mapTask()
+
+}
+
+fun mapTask(){
+
+    var dictionary = mapOf(
+      "eat" to "to consume food",
+      "sleep" to "a natural state of rest for the body and mind",
+      "food" to "any nutritious substance that people or animals eat or drink",
+      "repeat" to "to do something again"
+    )
+
+    println("Enter a word: ")
+    val word = readLine()
+    println("Meaning is: ${dictionary[word]}")
 
 }
