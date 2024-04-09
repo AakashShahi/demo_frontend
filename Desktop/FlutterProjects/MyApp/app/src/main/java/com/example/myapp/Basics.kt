c696d63e2764277b5d3fb4a4b9d739262fcf364e
AakashShahi <129369974+AakashShahi@users.noreply.github.com>
2024-04-09 09:53:03 +0545

    first learning

---
+++ Desktop/FlutterProjects/MyApp/app/src/main/java/com/example/myapp/Basics.kt
@@ -0,0 +1,13 @@
+package com.example.myapp
+
+fun main() {
+  var name: String="Ram"
+  var len: Any=name.length
+    println(len)
+  var a= arrayListOf<Int>(1,2,3,4)
+  println(a)
+  var b= ArrayList<Any>()
+  b.add(1)
+  b.add("ram")
+
+}
